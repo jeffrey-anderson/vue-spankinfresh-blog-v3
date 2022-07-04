c8d65c824238fe3e97acdac3f0dc064ffe9d6d3a
Jeff Anderson <jeff@jeff-anderson.com>
2022-07-04 16:26:12 -0400

    Get Okta config from .env files or environment variables

--- src/shared/config.js
+++ src/shared/config.js
@@ -1,7 +1,7 @@
 export const API = process.env.VUE_APP_API;
 
 // The Issuer URI from your Okta API Authorization Servers:
-export const OAUTH_ISSUER = 'enter-value-here';
+export const OAUTH_ISSUER = process.env.VUE_APP_OKTA_OAUTH_ISSUER;
 
 // From your Okta applications list, or on the "General" tab of a specific application:
-export const CLIENT_ID = 'enter-value-here';
+export const CLIENT_ID = process.env.VUE_APP_OKTA_CLIENT_ID;
